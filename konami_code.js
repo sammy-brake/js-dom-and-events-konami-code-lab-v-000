5aeedeb46971ba38d01d423f6170e3c6ee64c7fb
Samantha brake <sammy.brake@gmail.com>
2019-05-28 01:33:24 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-function onKeyDownHandler(e) {
+function init(e) {
   const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
