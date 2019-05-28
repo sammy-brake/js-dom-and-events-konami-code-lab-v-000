80f352db357967ff2583780f8dc17bc82c4b4cdc
Samantha brake <sammy.brake@gmail.com>
2019-05-28 01:32:52 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,13 +4,13 @@ let index = 0;
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
- 
+
   if (key === code[index]) {
     index++;
- 
+
     if (index === alphabet.length) {
       alert("Hurray!");
- 
+
       index = 0;
     }
   } else {
