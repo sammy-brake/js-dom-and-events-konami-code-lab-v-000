7987a2fa585a2e5179795cf570720aeb808d8f4e
Samantha brake <sammy.brake@gmail.com>
2019-05-28 01:57:44 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 
-document.body.addEventListener("keydown", function init(e) {
+function init(e) {
   const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
@@ -16,4 +16,4 @@ document.body.addEventListener("keydown", function init(e) {
   } else {
     index = 0;
   }
-});
+}
